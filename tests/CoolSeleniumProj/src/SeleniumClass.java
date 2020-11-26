413dfb299da6dd7f173f57672a3708d13d3bc8d8
nicholaskuhn <nicholaskuhn@mail.weber.edu>
2020-11-26 16:02:15 -0700

    updated tests

--- tests/CoolSeleniumProj/src/SeleniumClass.java
+++ tests/CoolSeleniumProj/src/SeleniumClass.java
@@ -1,6 +1,14 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,8 +22,8 @@ public class SeleniumClass {
 		System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://nearizpe.heliohost.us/");
-		driver.findElement(By.id("details-button")).click();
-		driver.findElement(By.id("proceed-link")).click();
+		getElementByEndingId("details-button").click();
+		getElementByEndingId("proceed-link").click();
 	}
 
 	@AfterClass
@@ -49,29 +57,125 @@ public class SeleniumClass {
 		sleepThread(3000);
 	}
 
+	/*
+	 * Test if you can login/logout as a user and if it appropriately locks other
+	 * users out from logging in while someone else is logged in
+	 */
 	@Test
 	public void testUserLogin() {
 		sleepThread(2000);
-		driver.findElement(By.id("User1")).click();
+		getElementByEndingId("User1").click();
 		sleepThread(500);
 		assertEquals("Successfully logged in!", getElementByEndingId("loginText").getText());
 		sleepThread(5000);
-		driver.findElement(By.id("User2")).click();
+		getElementByEndingId("User2").click();
 		sleepThread(500);
 		assertEquals("Sorry, Nick is still logged in", getElementByEndingId("loginText").getText());
 		sleepThread(5000);
-		driver.findElement(By.id("User1")).click();
+		getElementByEndingId("User1").click();
 		sleepThread(500);
 		assertEquals("Successfully logged out!", getElementByEndingId("loginText").getText());
 		sleepThread(2500);
 	}
-	
+
+	/*
+	 * Tests item insertion and auto completes
+	 */
 	@Test
 	public void testMuseumTab() {
-		
+		// getElementByEndingId("itemNameBox").click();
+		sleepThread(2000);
+		getElementByEndingId("User1").click();
+		sleepThread(2000);
+
+		getElementByEndingId("itemNameBox").sendKeys("testFossil");
+		sleepThread(1000);
+		getElementByEndingId("addItemBtn").click();
+		sleepThread(1000);
+		getElementByEndingId("bugRadio").click();
+		sleepThread(1000);
+		getElementByEndingId("itemNameBox").clear();
+		sleepThread(3000);
+
+		getElementByEndingId("itemNameBox").sendKeys("testBug");
+		sleepThread(1000);
+		getElementByEndingId("addItemBtn").click();
+		sleepThread(1000);
+		getElementByEndingId("fishRadio").click();
+		sleepThread(1000);
+		getElementByEndingId("itemNameBox").clear();
+		sleepThread(3000);
+
+		getElementByEndingId("itemNameBox").sendKeys("testFish");
+		sleepThread(1000);
+		getElementByEndingId("addItemBtn").click();
+		sleepThread(1000);
+		getElementByEndingId("paintingRadio").click();
+		sleepThread(1000);
+		getElementByEndingId("itemNameBox").clear();
+		sleepThread(3000);
+
+		getElementByEndingId("itemNameBox").sendKeys("testPainting");
+		sleepThread(1000);
+		getElementByEndingId("addItemBtn").click();
+		sleepThread(1000);
+		getElementByEndingId("itemNameBox").clear();
+		sleepThread(3000);
+
+		getElementByEndingId("itemNameBox").sendKeys("Test");
+		sleepThread(2000);
+		assertTrue(doesTextExistInTable("Testfossil"));
+		assertTrue(doesTextExistInTable("Testbug"));
+		assertTrue(doesTextExistInTable("Testfish"));
+		assertTrue(doesTextExistInTable("Testpainting"));
+
+		getElementByEndingId("User1").click();
 	}
 
-	public WebElement getElementByEndingId(String id) {
+	@Test
+	public void testCalendarTab() {
+
+		sleepThread(2000);
+		getElementByEndingId("User1").click();
+		sleepThread(2000);
+
+		getElementByEndingId("calendarTab").click();
+		sleepThread(2000);
+		getElementByEndingId("calendarDate").sendKeys("11/25/2020");
+		sleepThread(1000);
+		getElementByEndingId("calendarDate").sendKeys(Keys.TAB);
+		sleepThread(1000);
+		getElementByEndingId("openEventBtn").sendKeys(Keys.ENTER);
+		sleepThread(1000);
+		getElementByEndingId("eventNameBox").sendKeys("Test Event");
+		sleepThread(1000);
+		getElementByEndingId("eventDescriptionBox").sendKeys("Test Description Redd");
+		sleepThread(1000);
+		getElementByEndingId("addEventBtn").click();
+		sleepThread(3000);
+
+		assertEquals("Test Event", driver.findElement(By.xpath("//*[@id=\"calendarEvents\"]/div/h3")).getText());
+		assertEquals("https://nearizpe.heliohost.us/images/redd.png",
+				driver.findElement(By.xpath("//*[@id=\"calendarEvents\"]/div/img")).getAttribute("src"));
+		assertEquals("Test Description Redd",
+				driver.findElement(By.xpath("//*[@id=\"calendarEvents\"]/div/p")).getText());
+
+		getElementByEndingId("User1").click();
+		sleepThread(1000);
+	}
+
+	public boolean doesTextExistInTable(String text) {
+		WebElement table = getElementByEndingId("museumResults");
+		List<WebElement> tableDatas = table.findElements(By.tagName("td"));
+		for (WebElement data : tableDatas) {
+			if (data.getText().equals(text))
+				return true;
+		}
+
+		return false;
+	}
+
+	public static WebElement getElementByEndingId(String id) {
 		return driver.findElement(By.id(id));
 	}
 
@@ -83,7 +187,7 @@ public class SeleniumClass {
 		}
 		return false;
 	}
-	
+
 	public void sleepThread(int millis) {
 		try {
 			Thread.sleep(millis);
